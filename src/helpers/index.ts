c4b00c978bb676071a85629b7cd9a8b175e3d239
IsaacQH <isaacqwert@gmail.com>
2024-04-02 15:53:26 -0600

    Reiniciando form y cerrando modal

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,17 @@
+//Convierte el valor de dinero a una lectura más accesible
 export function formatCurrency(amount:number){
     return new Intl.NumberFormat('en-US', {style: 'currency', currency:'USD'}).format(amount)
+}
+
+
+//Convierte la fecha en un valor leible y personalizable
+export function formatDate(dateStr:string):string {
+    const dateObj = new Date(dateStr)  //Convertimos el input en fecha
+    const options: Intl.DateTimeFormatOptions = {  //Objeto de formato
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    }
+    return new Intl.DateTimeFormat('en-US', options).format(dateObj)   //Regresa la fecha
 }
