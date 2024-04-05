25c6ed5694cd485c1e47b1fe48a0e137172c70db
IsaacQH <isaacqwert@gmail.com>
2024-04-05 13:59:37 -0600

    New size

--- src/components/ExpenseDetail.tsx
+++ src/components/ExpenseDetail.tsx
@@ -48,7 +48,7 @@ export const ExpenseDetail = ({expense}: ExpenseDetailProp) => {
                 leadingActions={leadingActions()}
                 trailingActions={trailingActions()}
             >
-                <div className="bg-white shadow-lg p-10 w-full border-b border-gray-200 flex gap-5 items-center ">
+                <div className="bg-white shadow-lg py-10 w-full border-b border-gray-200 flex gap-5 items-center ">
                     <div>
                         <img src={`/icono_${categoryInfo.icon}.svg`} alt="icon image" className="w-20 h-20"/>
                     </div>
